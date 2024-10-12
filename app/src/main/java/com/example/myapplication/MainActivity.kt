4ec444c1ba201c5de6e51a55641112504564eee7
adievskiy <dievskiy@mail.ru>
2024-10-12 14:48:16 +0400

    Time Calculator

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.example.myapplication
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var toolbarMain: Toolbar
 
     private lateinit var firstTimeET: EditText
     private lateinit var secondTimeET: EditText
@@ -29,6 +36,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        toolbarMain = findViewById(R.id.toolbarMain)
+        setSupportActionBar(toolbarMain)
+        title = "Калькулятор времени"
+        toolbarMain.subtitle = "ver. 1.2"
+        toolbarMain.setLogo(R.drawable.ic_time)
+
         firstTimeET = findViewById(R.id.firstTimeET)
         secondTimeET = findViewById(R.id.secondTimeET)
 
@@ -39,6 +52,24 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         buttonSumBTN.setOnClickListener(this)
         buttonDifBTN.setOnClickListener(this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menumain, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.resetMenuMain -> {
+                firstTimeET.text.clear()
+                secondTimeET.text.clear()
+                resultTV.text = "Результат"
+            }
+            R.id.exitMenuMain -> finish()
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onClick(v: View) {
@@ -57,6 +88,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
             else -> "Ошибка"
         }
+        Toast.makeText(
+            applicationContext,
+            "Результат: $result",
+            Toast.LENGTH_LONG
+        ).show()
+        resultTV.setTextColor(Color.parseColor("#8B0000"))
         resultTV.text = result
     }
 }
